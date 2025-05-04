cc919e5816fd7c3c6d5de4038108e6872fe2c370
juansito12948 <elyuno72@gmail.com>
2025-05-04 18:56:38 +0000

    [🌙] Backup realizado! 2025-05-04

--- servidor_minecraft/run.sh
+++ servidor_minecraft/run.sh
@@ -3,4 +3,5 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.39/unix_args.txt "$@"
+#!/bin/bash
+java -Xms2G -Xmx6G @libraries/net/minecraftforge/forge/1.20.1-47.3.39/unix_args.txt "$@"
